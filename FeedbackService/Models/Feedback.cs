05159037983664a57a92f6178811306e7ee0e9d6
christofferPerch <cph-cn332@cphbusiness.dk>
2024-10-23 11:49:43 +0200

    Feedback model

---
+++ FeedbackService/Models/Feedback.cs
@@ -0,0 +1,15 @@
+﻿namespace FeedbackService.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; } 
+        public int CustomerId { get; set; } 
+        public int DeliveryAgentId { get; set; } 
+        public int FoodRating { get; set; } 
+        public int DeliveryExperienceRating { get; set; } 
+        public int DeliveryAgentRating { get; set; } 
+        public string Comments { get; set; } 
+        public DateTime FeedbackTimestamp { get; set; }
+    }
+}
